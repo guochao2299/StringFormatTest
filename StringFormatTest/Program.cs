58736e060c4be34c553ceefee8676a6d3c35de27
假面骑士龙骑 <szf.0325@gmail.com>
2020-05-30 20:21:04 +0800

    Add files via upload

---
+++ StringFormatTest/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringFormatTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string formatString = "part1{0}123{@}part2{1}456{@}part3{2}789{@}part4";
+            Console.WriteLine("格式化字符串：" + formatString);
+            try
+            {
+                //出错示例代码
+                //string resultString = string.Format(formatString, 0, 1, 2) ;                
+                //Console.WriteLine("ResultString:" + resultString);
+
+                //异常验证代码
+                Format(formatString, 0, 1, 2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static String Format(String format, params Object[] args)
+        {
+            return Format(null, format, args);
+        }
+
+        public static String Format(IFormatProvider provider, String format, params Object[] args)
+        {
+            if (format == null || args == null)
+                throw new ArgumentNullException((format == null) ? "format" : "args");
+            
+            AppendFormat(format, args);
+            return string.Empty;
+        }
+
+        private static void Append(char c)
+        { 
+        }
+
+        private static void Append(string s)
+        {
+        }
+
+        private static void Append(char c,int count)
+        {
+        }
+
+        private static  void AppendFormat(String format, params Object[] args)
+        {
+            if (format == null || args == null)
+            {
+                throw new ArgumentNullException((format == null) ? "format" : "args");
+            }          
+
+            int pos = 0;
+            int len = format.Length;
+            char ch = '\x0';
+
+            ICustomFormatter cf = null;            
+
+            while (true)
+            {
+                int p = pos;
+                int i = pos;
+                while (pos < len)
+                {
+                    ch = format[pos];
+
+                    pos++;
+                    if (ch == '}')
+                    {
+                        if (pos < len && format[pos] == '}') // Treat as escape character for }}
+                            pos++;
+                        else
+                            FormatError();
+                    }
+
+                    if (ch == '{')
+                    {
+                        if (pos < len && format[pos] == '{') // Treat as escape character for {{
+                            pos++;
+                        else
+                        {
+                            pos--;
+                            break;
+                        }
+                    }
+
+                    Append(ch);
+                }
+
+                if (pos == len) break;
+                pos++;
+                if (pos == len || (ch = format[pos]) < '0' || ch > '9')
+                {
+                    FormatError();
+                }
+                int index = 0;
+                do
+                {
+                    index = index * 10 + ch - '0';
+                    pos++;
+                    if (pos == len) FormatError();
+                    ch = format[pos];
+                } while (ch >= '0' && ch <= '9' && index < 1000000);
+                if (index >= args.Length) throw new IndexOutOfRangeException();
+                while (pos < len && (ch = format[pos]) == ' ') pos++;
+                bool leftJustify = false;
+                int width = 0;
+                if (ch == ',')
+                {
+                    pos++;
+                    while (pos < len && format[pos] == ' ') pos++;
+
+                    if (pos == len) FormatError();
+                    ch = format[pos];
+                    if (ch == '-')
+                    {
+                        leftJustify = true;
+                        pos++;
+                        if (pos == len) FormatError();
+                        ch = format[pos];
+                    }
+                    if (ch < '0' || ch > '9') FormatError();
+                    do
+                    {
+                        width = width * 10 + ch - '0';
+                        pos++;
+                        if (pos == len) FormatError();
+                        ch = format[pos];
+                    } while (ch >= '0' && ch <= '9' && width < 1000000);
+                }
+
+                while (pos < len && (ch = format[pos]) == ' ') pos++;
+                Object arg = args[index];
+                StringBuilder fmt = null;
+                if (ch == ':')
+                {
+                    pos++;
+                    p = pos;
+                    i = pos;
+                    while (true)
+                    {
+                        if (pos == len) FormatError();
+                        ch = format[pos];
+                        pos++;
+                        if (ch == '{')
+                        {
+                            if (pos < len && format[pos] == '{')  // Treat as escape character for {{
+                                pos++;
+                            else
+                                FormatError();
+                        }
+                        else if (ch == '}')
+                        {
+                            if (pos < len && format[pos] == '}')  // Treat as escape character for }}
+                                pos++;
+                            else
+                            {
+                                pos--;
+                                break;
+                            }
+                        }
+
+                        if (fmt == null)
+                        {
+                            fmt = new StringBuilder();
+                        }
+                        fmt.Append(ch);
+                    }
+                }
+                if (ch != '}') FormatError();
+                pos++;
+                String sFmt = null;
+                String s = null;
+                if (cf != null)
+                {
+                    if (fmt != null)
+                    {
+                        sFmt = fmt.ToString();
+                    }
+                    s = cf.Format(sFmt, arg, null) ;
+                }
+
+                if (s == null)
+                {
+                    IFormattable formattableArg = arg as IFormattable;
+
+#if FEATURE_LEGACYNETCF
+                    if(CompatibilitySwitches.IsAppEarlierThanWindowsPhone8) {
+                        // TimeSpan does not implement IFormattable in Mango
+                        if(arg is TimeSpan) {
+                            formattableArg = null;
+                        }
+                    }
+#endif
+                    if (formattableArg != null)
+                    {
+                        if (sFmt == null && fmt != null)
+                        {
+                            sFmt = fmt.ToString();
+                        }
+
+                        s = formattableArg.ToString(sFmt, null);
+                    }
+                    else if (arg != null)
+                    {
+                        s = arg.ToString();
+                    }
+                }
+
+                if (s == null) s = String.Empty;
+                int pad = width - s.Length;
+                if (!leftJustify && pad > 0) Append(' ', pad);
+                Append(s);
+                if (leftJustify && pad > 0) Append(' ', pad);
+            }
+        }
+
+        private static void FormatError()
+        {
+            throw new FormatException();
+        }
+    }
+}
